22d0973b5fe61e33e3f9d72c721b0317c5f7286b
Dmitry Kotlyarov <dmktv@microservice.systems>
2021-08-19 00:06:43 +0300

    added Maven project, Git ignore & base class

--- src/main/java/systems/microservice/loghub/maven/LogHubMojo.java
+++ src/main/java/systems/microservice/loghub/maven/LogHubMojo.java
@@ -1,8 +1,50 @@
+/*
+ * Copyright (C) 2020 Microservice Systems, Inc.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package systems.microservice.loghub.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class LogHubMojo {
+@Mojo(name = "properties", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, threadSafe = true)
+public class LogHubMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project.build.resources[0].directory}", required = true, readonly = true)
+    private String resourceDirectory;
+
+    @Parameter(defaultValue = "${project.artifactId}", required = true, readonly = true)
+    private String artifactId;
+
+    @Parameter(defaultValue = "${project.version}", required = true, readonly = true)
+    private String version;
+
+    private long buildTime;
+
+    public LogHubMojo() {
+    }
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+    }
 }
